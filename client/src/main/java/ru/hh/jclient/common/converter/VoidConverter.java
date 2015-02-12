a579b9d63cdc0f05a6d2a5e206bad2b613915d99
Nick Voznesenskiy <voznesenskiy@hh.ru>
2015-02-12 18:27:30 +0300

    HH-48572 VoidConverter should not check content type at all

--- client/src/main/java/ru/hh/jclient/common/converter/VoidConverter.java
+++ client/src/main/java/ru/hh/jclient/common/converter/VoidConverter.java
@@ -1,25 +1,13 @@
 package ru.hh.jclient.common.converter;
 
-import static com.google.common.collect.ImmutableSet.of;
-import static com.google.common.net.MediaType.ANY_TYPE;
-import java.util.Collection;
-import java.util.Set;
 import ru.hh.jclient.common.ResultWithResponse;
 import ru.hh.jclient.common.util.MoreFunctionalInterfaces.FailableFunction;
-import com.google.common.net.MediaType;
 import com.ning.http.client.Response;
 
-public class VoidConverter extends SingleTypeConverter<Void> {
-
-  private static final Set<MediaType> MEDIA_TYPES = of(ANY_TYPE);
+public class VoidConverter implements TypeConverter<Void> {
 
   @Override
-  public FailableFunction<Response, ResultWithResponse<Void>, Exception> singleTypeConverterFunction() {
+  public FailableFunction<Response, ResultWithResponse<Void>, Exception> converterFunction() {
     return r -> new ResultWithResponse<>(null, r);
-  }
-
-  @Override
-  public Collection<MediaType> getMediaTypes() {
-    return MEDIA_TYPES;
   }
 }
