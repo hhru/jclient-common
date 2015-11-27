abe33bf4147a84d44cee48fc68d889fff33e5f2d
Nick Voznesenskiy <voznesenskiy@hh.ru>
2015-11-27 18:59:04 +0300

    HH-56660 Move annotations to separate module

--- client-utils/src/main/java/ru/hh/jclient/common/JClient.java
+++ client-utils/src/main/java/ru/hh/jclient/common/JClient.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotates resource method that is called by one or more jclients. {@link #value()} is used to specify constant that will be also used in
+ * {@link JResource} on the jclient side.
+ *
+ * Constant can be searched for usages to reveal which resource and jclients are linked.
+ */
 @Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JClient {
--- client-utils/src/main/java/ru/hh/jclient/common/JResource.java
+++ client-utils/src/main/java/ru/hh/jclient/common/JResource.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotates jclient method that calls one or more resources. {@link #value()} is used to specify constant(s) that will be also used in
+ * {@link JClient} on the resource side.
+ *
+ * Constant can be searched for usages to reveal which resource and jclients are linked.
+ */
 @Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JResource {
