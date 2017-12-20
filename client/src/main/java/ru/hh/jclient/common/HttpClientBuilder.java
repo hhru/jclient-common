990d2ecd115204f115665949898dfee87231e6be
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2017-12-20 14:12:07 +0300

    HH-75244 adds wizard to build builder)

--- client/src/main/java/ru/hh/jclient/common/HttpClientBuilder.java
+++ client/src/main/java/ru/hh/jclient/common/HttpClientBuilder.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Executor;
 
+//TODO rename to HttpClientFactory
 public class HttpClientBuilder {
 
   private final AsyncHttpClient http;
