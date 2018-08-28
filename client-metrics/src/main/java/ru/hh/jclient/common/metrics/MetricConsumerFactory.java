962c55379506bd4ba9631095d719f2a4b32929bc
andrew <an.sumin@hh.ru>
2018-08-28 14:27:40 +0300

    HH-82522 add MetricConsumerFactory

---
+++ client-metrics/src/main/java/ru/hh/jclient/common/metrics/MetricConsumerFactory.java
@@ -0,0 +1,26 @@
+package ru.hh.jclient.common.metrics;
+
+import com.timgroup.statsd.StatsDClient;
+import ru.hh.jclient.common.metric.MetricConsumer;
+
+import java.util.Properties;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Optional.ofNullable;
+
+public class MetricConsumerFactory {
+  private MetricConsumerFactory() {}
+
+  private static MetricConsumer NOOP_METRIC_CONSUMER = metricProvider -> {};
+
+  public static MetricConsumer buildMetricConsumerFromProperties(Properties properties, String name, StatsDClient statsDClient, ScheduledExecutorService scheduler) {
+    if (!ofNullable(properties.getProperty("enabled")).map(Boolean::parseBoolean).orElse(Boolean.FALSE)) {
+      return NOOP_METRIC_CONSUMER;
+    }
+
+    return ofNullable(properties.getProperty("sendIntervalMs")).map(Integer::parseInt)
+      .<MetricConsumer>map(sendIntervalMs -> new StatsDMetricConsumer(name, statsDClient, scheduler, sendIntervalMs, TimeUnit.MILLISECONDS))
+      .orElse(NOOP_METRIC_CONSUMER);
+  }
+}
