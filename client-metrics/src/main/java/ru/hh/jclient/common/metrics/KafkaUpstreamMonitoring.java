836d8d2cf1dc4e79e61800cb75bfb845b856958d
Vladislav Senin <troy4eg@gmail.com>
2019-03-25 14:41:30 +0300

    HH-90249 refactored topics settings for KafkaUpstreamMonitoring

--- client-metrics/src/main/java/ru/hh/jclient/common/metrics/KafkaUpstreamMonitoring.java
+++ client-metrics/src/main/java/ru/hh/jclient/common/metrics/KafkaUpstreamMonitoring.java
@@ -14,17 +14,21 @@ import java.util.concurrent.TimeUnit;
 
 public class KafkaUpstreamMonitoring implements Monitoring {
   private static final Logger log = LoggerFactory.getLogger(KafkaUpstreamMonitoring.class);
-  private static final String METRICS_TOPIC = "metrics";
-  private static final String HEARTBEAT_TOPIC = "heartbeat";
+  private final String requestsCountTopicName;
+  private final String heartbeatTopicName;
 
   private final KafkaProducer<String, String> kafkaProducer;
   private final String serviceName;
   private final String dc;
 
-  public KafkaUpstreamMonitoring(KafkaProducer<String, String> kafkaMetricsProducer, String dc, String serviceName) {
+  public KafkaUpstreamMonitoring(KafkaProducer<String, String> kafkaMetricsProducer,
+                                 String dc, String serviceName,
+                                 String requestsCountTopicName, String heartbeatTopicName) {
     this.serviceName = serviceName;
     this.dc = dc;
     this.kafkaProducer = kafkaMetricsProducer;
+    this.requestsCountTopicName = requestsCountTopicName;
+    this.heartbeatTopicName = heartbeatTopicName;
   }
 
   public void startHeartbeat(ScheduledExecutorService executorService, long periodInMillis) throws UnknownHostException {
@@ -36,7 +40,7 @@ public class KafkaUpstreamMonitoring implements Monitoring {
       jsonBuilder.put("hostname", hostname);
       jsonBuilder.put("dc", dc);
       jsonBuilder.put("ts", TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-      ProducerRecord<String, String> record = new ProducerRecord<>(HEARTBEAT_TOPIC, jsonBuilder.build());
+      ProducerRecord<String, String> record = new ProducerRecord<>(heartbeatTopicName, jsonBuilder.build());
       kafkaProducer.send(record, (recordMetadata, e) -> log.warn(e.getMessage(), e));
     }, 0, periodInMillis, TimeUnit.MILLISECONDS);
   }
@@ -51,7 +55,7 @@ public class KafkaUpstreamMonitoring implements Monitoring {
     jsonBuilder.put("hostname", serverAddress);
     jsonBuilder.put("ts", TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
     jsonBuilder.put("requestId", requestId);
-    ProducerRecord<String, String> record = new ProducerRecord<>(METRICS_TOPIC, jsonBuilder.build());
+    ProducerRecord<String, String> record = new ProducerRecord<>(requestsCountTopicName, jsonBuilder.build());
     kafkaProducer.send(record, (recordMetadata, e) -> log.warn(e.getMessage(), e));
   }
 
