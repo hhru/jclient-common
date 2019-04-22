0433ae637e2982bdd954d313dd3507921325b4d2
Andrew Sumin <sumin.andrew@gmail.com>
2019-04-22 15:40:48 +0300

    HH-91499 move BalancingUpstreamManagerFactory to jclient-common

--- client-metrics/src/main/java/ru/hh/jclient/common/metrics/KafkaUpstreamMonitoring.java
+++ client-metrics/src/main/java/ru/hh/jclient/common/metrics/KafkaUpstreamMonitoring.java
@@ -2,6 +2,7 @@ package ru.hh.jclient.common.metrics;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -9,6 +10,8 @@ import ru.hh.jclient.common.Monitoring;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Optional;
+import java.util.Properties;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -17,11 +20,16 @@ import static java.util.Optional.ofNullable;
 public class KafkaUpstreamMonitoring implements Monitoring {
   private static final Logger log = LoggerFactory.getLogger(KafkaUpstreamMonitoring.class);
 
+  private final String serviceName;
+  private final String localDc;
   private final KafkaProducer<String, String> kafkaProducer;
-  private final KafkaUpstreamMonitoringConfig monitoringConfig;
+  private final Config monitoringConfig;
 
-  public KafkaUpstreamMonitoring(KafkaProducer<String, String> kafkaMetricsProducer,
-                                 KafkaUpstreamMonitoringConfig monitoringConfig) {
+  public KafkaUpstreamMonitoring(String serviceName, String localDc,
+                                 KafkaProducer<String, String> kafkaMetricsProducer,
+                                 Config monitoringConfig) {
+    this.serviceName = serviceName;
+    this.localDc = localDc;
     this.kafkaProducer = kafkaMetricsProducer;
     this.monitoringConfig = monitoringConfig;
   }
@@ -32,9 +40,9 @@ public class KafkaUpstreamMonitoring implements Monitoring {
     executorService.scheduleAtFixedRate(() -> {
       var jsonBuilder = new SimpleJsonBuilder();
       jsonBuilder.put("ts", TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-      jsonBuilder.put("app", monitoringConfig.serviceName);
+      jsonBuilder.put("app", serviceName);
       jsonBuilder.put("hostname", hostname);
-      jsonBuilder.put("dc", monitoringConfig.dc);
+      jsonBuilder.put("dc", localDc);
       ProducerRecord<String, String> record = new ProducerRecord<>(monitoringConfig.heartbeatTopicName, jsonBuilder.build());
       kafkaProducer.send(record, (recordMetadata, e) -> {
         if (e != null) {
@@ -50,7 +58,7 @@ public class KafkaUpstreamMonitoring implements Monitoring {
       var requestId = ofNullable(MDC.get("rid")).orElse("");
       var jsonBuilder = new SimpleJsonBuilder();
       jsonBuilder.put("ts", TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-      jsonBuilder.put("app", monitoringConfig.serviceName);
+      jsonBuilder.put("app", serviceName);
       jsonBuilder.put("upstream", upstreamName);
       jsonBuilder.put("dc", dc);
       jsonBuilder.put("server", serverAddress);
@@ -75,4 +83,31 @@ public class KafkaUpstreamMonitoring implements Monitoring {
 
   }
 
+  public static Optional<KafkaUpstreamMonitoring> fromProperties(String serviceName, String dc, Properties properties) {
+    return ofNullable(properties)
+      .map(props -> props.getProperty("enabled")).map(Boolean::parseBoolean)
+      .filter(Boolean.TRUE::equals)
+      .map(b -> new KafkaProducer<>(properties, new StringSerializer(), new StringSerializer()))
+      .map(producer -> new KafkaUpstreamMonitoring(serviceName, dc, producer, Config.fromProperties(properties)));
+  }
+
+  static class Config {
+    final long heartbeatPeriodInMillis;
+    final String heartbeatTopicName;
+    final String requestsCountTopicName;
+
+    Config(long heartbeatPeriodInMillis, String heartbeatTopicName, String requestsCountTopicName) {
+      this.heartbeatPeriodInMillis = heartbeatPeriodInMillis;
+      this.heartbeatTopicName = heartbeatTopicName;
+      this.requestsCountTopicName = requestsCountTopicName;
+    }
+
+    static Config fromProperties(Properties properties) {
+      var requestsCountTopicName = properties.getProperty("topics.requests");
+      var heartbeatTopicName = properties.getProperty("topics.heartbeat");
+      var heartbeatPeriodInMillis = Long.parseLong(properties.getProperty("heartbeat.period.ms", "10000"));
+
+      return new Config(heartbeatPeriodInMillis, heartbeatTopicName, requestsCountTopicName);
+    }
+  }
 }
