664384fa07902a2bc240b76cebf1fb7cf6e26300
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2019-08-15 16:26:59 +0300

    HH-95214 more obvious api for query params

---
+++ client-tests/src/test/java/ru/hh/jclient/common/JClientBaseTest.java
@@ -0,0 +1,26 @@
+package ru.hh.jclient.common;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class JClientBaseTest {
+
+  private JClientBase jClientBase;
+
+  @Before
+  public void setUp() {
+    jClientBase = new JClientBase("test", mock(HttpClientFactory.class)) {};
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testVarargsNotEven() {
+    jClientBase.build("GET", "http://test", "a", "b", "c");
+  }
+
+  @Test(expected = ClassCastException.class)
+  public void testVarargsNotStringKey() {
+    jClientBase.build("GET", "http://test", 1, "2");
+  }
+}
