167a31ad35990a847bf93356d252ef64de6745f7
Kazantsev Alexander <al.kazantsev@hh.ru>
2020-03-18 14:46:39 +0300

    HH-76429 fix naming http constants

--- jclient-common-api/src/main/java/ru/hh/jclient/common/HttpStatuses.java
+++ jclient-common-api/src/main/java/ru/hh/jclient/common/HttpStatuses.java
@@ -1,26 +1,14 @@
 package ru.hh.jclient.common;
 
-public interface HttpStatuses {
-  int SC_OK = 200;
-  int SC_CREATED = 201;
-  int SC_NO_CONTENT = 204;
-  int SC_BAD_REQUEST = 400;
+public final class HttpStatuses {
+  public static final int OK = 200;
+  public static final int CREATED = 201;
+  public static final int NO_CONTENT = 204;
 
-  int BAD_GATEWAY = 502;
-  int SERVICE_UNAVAILABLE = 503;
-  int GATEWAY_TIMEOUT = 504;
+  public static final int BAD_REQUEST = 400;
 
-  int CONNECT_TIMEOUT_ERROR = 599;
-
-  /**
-   * use CONNECT_TIMEOUT_ERROR
-   */
-  @Deprecated
-  int CONNECT_ERROR = 599;
-
-  /**
-   * use CONNECT_TIMEOUT_ERROR
-   */
-  @Deprecated
-  int REQUEST_TIMEOUT = 599;
+  public static final int BAD_GATEWAY = 502;
+  public static final int SERVICE_UNAVAILABLE = 503;
+  public static final int GATEWAY_TIMEOUT = 504;
+  public static final int CONNECT_TIMEOUT_ERROR = 599;
 }
