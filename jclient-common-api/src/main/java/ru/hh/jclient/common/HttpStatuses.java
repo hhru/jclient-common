9763c07e783587c9c25d3cfcb411b39e8153eba1
Kazantsev Alexander <al.kazantsev@hh.ru>
2020-03-18 11:56:21 +0300

    HH-76429 add new HttpStatuses constants

--- jclient-common-api/src/main/java/ru/hh/jclient/common/HttpStatuses.java
+++ jclient-common-api/src/main/java/ru/hh/jclient/common/HttpStatuses.java
@@ -1,10 +1,26 @@
 package ru.hh.jclient.common;
 
-public class HttpStatuses {
-  public static final int BAD_GATEWAY = 502;
-  public static final int SERVICE_UNAVAILABLE = 503;
-  public static final int GATEWAY_TIMEOUT = 504;
+public interface HttpStatuses {
+  int SC_OK = 200;
+  int SC_CREATED = 201;
+  int SC_NO_CONTENT = 204;
+  int SC_BAD_REQUEST = 400;
 
-  public static final int CONNECT_ERROR = 599;
-  public static final int REQUEST_TIMEOUT = 599;
+  int BAD_GATEWAY = 502;
+  int SERVICE_UNAVAILABLE = 503;
+  int GATEWAY_TIMEOUT = 504;
+
+  int CONNECT_TIMEOUT_ERROR = 599;
+
+  /**
+   * use CONNECT_TIMEOUT_ERROR
+   */
+  @Deprecated
+  int CONNECT_ERROR = 599;
+
+  /**
+   * use CONNECT_TIMEOUT_ERROR
+   */
+  @Deprecated
+  int REQUEST_TIMEOUT = 599;
 }
