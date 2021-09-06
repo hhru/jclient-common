08e6c9712f21c3a653731577ce80a74c48410b5e
d.zharikhin <d.zharikhin@hh.ru>
2021-09-06 18:40:26 +0300

    HH-134923 explain public morozov pattern

--- client-tests/src/test/java/ru/hh/jclient/common/balancing/UpstreamMorozov.java
+++ client-tests/src/test/java/ru/hh/jclient/common/balancing/UpstreamMorozov.java
@@ -2,6 +2,7 @@ package ru.hh.jclient.common.balancing;
 
 import java.util.List;
 
+// to make constructor available outside of the package to housekeep tests better
 public class UpstreamMorozov extends Upstream {
 
   public UpstreamMorozov(String upstreamName, UpstreamConfigs upstreamConfigs, List<Server> servers, String datacenter,
