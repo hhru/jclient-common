273b37400f2aeb41420d18f59297c2fd91bdae51
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2024-02-28 18:08:03 +0300

    HH-208533 fix sending disabled upstreams to okmeter

--- balancing/src/main/java/ru/hh/jclient/common/balancing/RequestBalancerBuilder.java
+++ balancing/src/main/java/ru/hh/jclient/common/balancing/RequestBalancerBuilder.java
@@ -41,7 +41,7 @@ public class RequestBalancerBuilder implements RequestEngineBuilder<RequestBalan
     }
     if (upstream == null || !upstream.isEnabled()) {
       int maxTimeoutTries = Optional.ofNullable(this.maxTimeoutTries).orElseGet(UpstreamConfig.DEFAULT_CONFIG::getMaxTimeoutTries);
-      return new ExternalUrlRequestor(request, requestExecutor,
+      return new ExternalUrlRequestor(upstream, request, requestExecutor,
         requestExecutor.getDefaultRequestTimeoutMs(), maxTimeoutTries, UpstreamConfig.DEFAULT_CONFIG.getMaxTries(),
         timeoutMultiplier, balancingRequestsLogLevel, forceIdempotence, monitoring);
     } else {
