e40245e1abb4163ae727490aa82552b8ab7b94bb
Roman Kozlov <r.kozlov@hh.ru>
2025-06-16 13:24:12 +0300

    HH-264234 remove fallback to non-adaptive

--- balancing/src/main/java/ru/hh/jclient/common/balancing/AdaptiveBalancingState.java
+++ balancing/src/main/java/ru/hh/jclient/common/balancing/AdaptiveBalancingState.java
@@ -2,15 +2,9 @@ package ru.hh.jclient.common.balancing;
 
 import java.util.Iterator;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AdaptiveBalancingState extends BalancingState {
-  private static final Logger LOGGER = LoggerFactory.getLogger(AdaptiveBalancingState.class);
-
   private Iterator<ServerEntry> serverEntryIterator;
-  private boolean adaptiveFailed;
-
 
   public AdaptiveBalancingState(Upstream upstream, String profile) {
     super(upstream, profile);
@@ -18,22 +12,13 @@ public class AdaptiveBalancingState extends BalancingState {
 
   @Override
   public void acquireServer() {
-    if (!adaptiveFailed) {
-      try {
-        setCurrentServer(acquireAdaptiveServer());
-        return;
-      } catch (RuntimeException e) {
-        LOGGER.error("failed to acquire adaptive servers, falling back to nonadaptive", e);
-        adaptiveFailed = true;
-      }
-    }
-    super.acquireServer();
+    setCurrentServer(acquireAdaptiveServer());
   }
 
   @Override
   public void releaseServer(long timeToLastByteMillis, boolean isServerError) {
     if (isServerAvailable()) {
-      upstream.releaseServer(getCurrentServer().getIndex(), !getTriedServers().isEmpty(), isServerError, timeToLastByteMillis, !adaptiveFailed);
+      upstream.releaseServer(getCurrentServer().getIndex(), !getTriedServers().isEmpty(), isServerError, timeToLastByteMillis, true);
     }
   }
 
