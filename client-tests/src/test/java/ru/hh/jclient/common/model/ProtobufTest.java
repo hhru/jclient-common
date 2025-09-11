bcdcaf4fecbbcea9f595d35a684653c16e421930
Pavel Orlov <p.orlov@hh.ru>
2025-09-11 19:37:55 +0300

    HH-275689 fix deprecations

--- client-tests/src/test/java/ru/hh/jclient/common/model/ProtobufTest.java
+++ client-tests/src/test/java/ru/hh/jclient/common/model/ProtobufTest.java
@@ -561,7 +561,7 @@ public final class ProtobufTest {
       getDescriptor() {
     return descriptor;
   }
-  private static  com.google.protobuf.Descriptors.FileDescriptor
+  private static final com.google.protobuf.Descriptors.FileDescriptor
       descriptor;
   static {
     java.lang.String[] descriptorData = {
@@ -569,18 +569,12 @@ public final class ProtobufTest {
       "roto\"\"\n\023ProtobufTestMessage\022\013\n\003ids\030\001 \003(\005" +
       "B\034\n\032ru.hh.jclient.common.model"
     };
-    com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
-        new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
-          public com.google.protobuf.ExtensionRegistry assignDescriptors(
-              com.google.protobuf.Descriptors.FileDescriptor root) {
-            descriptor = root;
-            return null;
-          }
-        };
-    com.google.protobuf.Descriptors.FileDescriptor
-      .internalBuildGeneratedFileFrom(descriptorData,
-        new com.google.protobuf.Descriptors.FileDescriptor[] {
-        }, assigner);
+
+    descriptor = com.google.protobuf.Descriptors.FileDescriptor.internalBuildGeneratedFileFrom(
+        descriptorData,
+        new com.google.protobuf.Descriptors.FileDescriptor[] {}
+    );
+
     internal_static_ProtobufTestMessage_descriptor =
       getDescriptor().getMessageTypes().get(0);
     internal_static_ProtobufTestMessage_fieldAccessorTable = new
